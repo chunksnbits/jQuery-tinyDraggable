630235619f3622e3da5af0cb826722e384962910
Daniel Eissing <daniel.eissing@sinnerschrader.com>
2015-05-22 11:02:21 +0200

    Adds contain option for containing dragged element within window bounds.

--- jquery.tiny-draggable.js
+++ jquery.tiny-draggable.js
@@ -1,38 +1,58 @@
 /*;
-	jQuery tinyDraggable v1.0.3
+	jQuery tinyDraggable v1.0.4
+    Copyright (c) 2015 Daniel Eissing / chunknsbits
     Copyright (c) 2014 Simon Steinberger / Pixabay
-    GitHub: https://github.com/Pixabay/jQuery-tinyDraggable
-    More info: http://pixabay.com/blog/posts/p-52/
+    GitHub: https://github.com/chunksnbits/jQuery-tinyDraggable/tree/feature/contain
+    Forked from: https://github.com/pixabay/jQuery-tinyDraggable
 	License: http://www.opensource.org/licenses/mit-license.php
 */
 
 (function($){
-    $.fn.tinyDraggable = function(options){
-        var settings = $.extend({ handle: 0, exclude: 0 }, options);
-        return this.each(function(){
-            var dx, dy, el = $(this), doc = $(document), handle = settings.handle ? $(settings.handle, el) : el;
-            handle.on({
-                mousedown: function(e){
-                    if (settings.exclude && ~$.inArray(e.target, $(settings.exclude, el))) return;
-                    if (settings.callback && settings.callback('start', e.pageX, e.pageY) === false) return;
+  $.fn.tinyDraggable = function(options){
+    var settings = $.extend({ handle: 0, exclude: 0, contain: 0 }, options);
+    return this.each(function(){
+      var x, y, dx, dy, el = $(this), doc = $(document), win = $(window), handle = settings.handle ? $(settings.handle, el) : el;
 
-                    e.preventDefault();
-                    var os = el.offset(); dx = e.pageX-os.left, dy = e.pageY-os.top;
+      handle.on({
+        mousedown: function(e){
+          if (settings.exclude && ~$.inArray(e.target, $(settings.exclude, el))) return;
+          if (settings.callback && settings.callback('start', e.pageX, e.pageY) === false) return;
 
-                    function onDrag(e){
-                      var x = e.pageX-dx, y = e.pageY-dy;
-                      if (!settings.callback || settings.callback('drag', x, y) !== false) {
-                        el.offset({top: y, left: x});
-                      }
-                    }
-                    function onStop(e){
-                      if (settings.callback && settings.callback('stop', e.pageX, e.pageY) === false) return;
-                      doc.off('mousemove.drag', onDrag).off('mouseup', onStop);
-                    }
+          e.preventDefault();
+          var os = el.offset(), dx = e.pageX-os.left, dy = e.pageY-os.top;
 
-                    doc.on('mousemove.drag', onDrag).on('mouseup', onStop);
-                }
-            });
-        });
-    };
+          var bounds = (function () {
+            if (settings.contain) {
+              el.css({ position: 'fixed' });
+
+              return {
+                x: win.width() - el.width(),
+                y: win.height() - el.height()
+              };
+            }
+
+            return null;
+          })();
+
+          function onDrag(e){
+            x = settings.contain ? Math.max(0, Math.min(bounds.x, e.clientX-dx)) : e.pageX-dx;
+            y = settings.contain ? Math.max(0, Math.min(bounds.y, e.clientY-dy)) : e.pageY-dy;
+
+            if (!settings.callback || settings.callback('drag', x, y) !== false) {
+              if (settings.contain) {
+                return el.css({top: y, left: x});
+              }
+              return el.offset({ top: y, left: x });
+            }
+          }
+          function onStop(){
+            if (settings.callback && settings.callback('stop', x, y) === false) return;
+            doc.off('mousemove.drag', onDrag).off('mouseup', onStop);
+          }
+
+          doc.on('mousemove.drag', onDrag).on('mouseup', onStop);
+        }
+      });
+    });
+  };
 }(jQuery));
